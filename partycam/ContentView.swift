e0743cd0540a56d9ef9e4fd4d896f59e9d8e34a8
Joseph Lyon <joeylyon1@gmail.com>
2025-05-09 07:19:20 -0400

    Initial Commit

---
+++ partycam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  partycam
+//
+//  Created by Joey Lyon on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ partycam/partycamApp.swift
@@ -0,0 +1,17 @@
+//
+//  partycamApp.swift
+//  partycam
+//
+//  Created by Joey Lyon on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct partycamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
